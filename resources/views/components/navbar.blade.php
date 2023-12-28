5bd840d62aeaeb85c866065c43d04589b740cd38
Jose <jcarlossoorro@gmail.com>
2023-12-28 07:43:20 -0500

    Add navbar prototype

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,23 @@
-{{-- code for navbar goes here --}}
 
-<nav>
-    <div> logo </div>
-    <a href="#">Sign up</a>
-    <a href="#"> Log in</a>
+<nav class="flex justify-between px-24 py-2 border">
+    <div class="left-section flex items-center gap-6">
+        <div class="logo flex items-center gap-2">
+            <img src="/images/kajomart.png" alt="logo" class="w-12">
+            <p class="text-3xl bold">KajoMart</p>
+        </div>
+
+        <form class="flex gap-0">
+            <input type="text" name="search" id="" class="rounded-l-md border h-10 px-4 py-2 focus:outline-none " placeholder="Search for anything...">
+            <button type="submit" class="bg-green-700 rounded-r-md px-4 py-2 h-10 "> <i class="fa fa-search text-white" ></i> </button>
+        </form>
+    </div>
+
+
+
+    <div class="flex items-center gap-4">
+        <a href="" class="text-xl hover:text-green-500 hover:underline">Teach on KajoMart</a>
+        <a href="" class="text-xl hover:text-green-500 hover:underline">My Learning</a>
+        <img src="" alt="profile picture" class="rounded-full border bg-slate-300 w-12 h-12" >
+    </div>
+
 </nav>
